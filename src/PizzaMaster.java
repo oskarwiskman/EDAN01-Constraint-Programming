25e9e4ddbe2b16797b103f4e3a3c7f053d0ca623
christineboghammar <christine@boghammar.se>
2016-01-26 15:00:58 +0100

    execution time added

--- src/PizzaMaster.java
+++ src/PizzaMaster.java
@@ -1,6 +1,19 @@
+import org.jacop.core.Store;
+
 /**
  * Created by oskar on 1/26/16.
  */
 public class PizzaMaster {
-    
+	public static void main(String[] args) {
+		long T1, T2, T;
+		T1 = System.currentTimeMillis();
+		letsDeal();
+		T2 = System.currentTimeMillis();
+		T = T2 - T1;
+		System.out.println("\n\t*** Execution time = " + T + " ms");
+	}
+private static void letsDeal() {
+		Store store = new Store();	
+		
+	}
 }
